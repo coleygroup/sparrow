9dfab8df0613d097b38aeb1cbb13631c47bbd380
jenna-fromer <jenna.fromer@gmail.com>
2023-06-06 16:20:47 -0400

    add debugging files

--- debug/route_graph_debug.py
+++ debug/route_graph_debug.py
@@ -1,20 +1,25 @@
 from mars.route_graph import RouteGraph
 from mars.route_selector import RouteSelector
+from mars.visualizer import Visualizer
 import pickle 
 
 
-with open('simple_tree.pickle', 'rb') as file:
-    paths = pickle.load(file)
+# with open('debug/simple_tree.pickle', 'rb') as file:
+    # paths = pickle.load(file)
 
-route_graph = RouteGraph(paths=paths)
+# route_graph = RouteGraph(paths=paths)
+
+with open('debug/RouteGraph_withScores.pickle', 'rb') as file: 
+    route_graph = pickle.load(file)
 
 target_dict = {
-    'O=C1CN=C(c2ccccn2)c2cc(Br)ccc2N1': 100000
+    'O=C1CN=C(c2ccccn2)c2cc(Br)ccc2N1': 10
 }
 
 route_selector = RouteSelector(
     route_graph,
     target_dict,
+    calc_reaction_scores=False
 )
 
 route_selector.define_variables()
@@ -22,5 +27,9 @@ route_selector.set_objective()
 route_selector.set_constraints()
 
 route_selector.optimize()
+
+nonzero_vars = route_selector.routes_from_optimal_variables()
+
+visualizer = Visualizer(route_graph=route_selector.graph, nonzero_vars=nonzero_vars)
 
 print('Optimization problem done')
---
+++ debug/visualize_debug.py
@@ -0,0 +1,8 @@
+import pickle 
+
+
+with open('debug/Simple_RouteGraph_after_constraints.pickle', 'rb') as file:
+    route_graph = pickle.load(file)
+
+
+print('done')
