b2640ec4e3f076a2e3d87a13a717c91b55f3245d
jenna-fromer <jenna.fromer@gmail.com>
2023-07-07 17:04:41 -0400

    Add separate classes for reaction scoring and recommending conditions, for flexibility

---
+++ src/sparrow/condition_recommender.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod, abstractproperty
+from typing import List 
+
+class Recommender(ABC): 
+    """ 
+    Recommends reaction conditions (i.e. contexts) from a reaction smiles.
+    """
+
+    @abstractmethod
+    def recommend_conditions(rxn_smi: str, n_c: int) -> List: 
+        """ Returns a list of n_c recommended conditions (each a list). """
+
+
+class AskcosRecommender(): 
+    """ Uses ASKCOS's context recommender to predict conditions """
+    
+    def __init__(self): 
+
+        from askcos.synthetic.context.neuralnetwork import NeuralNetContextRecommender
+        import askcos.global_config as gc
+        import askcos.utilities.contexts as context_cleaner
+        
+        self.recommender = NeuralNetContextRecommender()
+        self.recommender.load_nn_model(
+                model_path=gc.NEURALNET_CONTEXT_REC['model_path'], 
+                info_path=gc.NEURALNET_CONTEXT_REC['info_path'], 
+                weights_path=gc.NEURALNET_CONTEXT_REC['weights_path']
+            )
+        
+        self.cleaner = context_cleaner
+        
+        return 
+    
+    def recommend_conditions(self, rxn_smi: str, n_c: int = 1) -> List: 
+        
+        conditions = self.get_conditions(rxn_smi, n_c)
+        clean_conditions = [self.clean_condition[cond] for cond in conditions]
+
+        return clean_conditions
+    
+    def __call__(self, rxn_smi: str, n_c: int = 1) -> List: 
+
+        return self.recommend_conditions(rxn_smi, n_c)
+        
+
+    def get_conditions(self, rxn_smi: str, n_c: int = 1) -> List: 
+        conditions = self.recommender.get_n_conditions(
+            rxn_smi, 
+            n=n_c, 
+            return_separate=True, 
+            return_scores=False
+        )
+
+        return conditions
+
+    def clean_condition(self, condition: List) -> List:
+        """ 
+        Takes an 'uncleaned' condition that has temperatures and empty entries
+        and returns a 'cleaned' context with only chemical 
+        """
+        conditions = self.cleaner.clean_context(condition[:4])
+
+    def __call__(self, rxn_smi: str, n_c: int = 1): 
+        
+        return self.recommend_conditions(rxn_smi, n_c)
+
+
---
+++ src/sparrow/scorer.py
@@ -0,0 +1,64 @@
+""" A Scorer gives one or more scores to a reaction """
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Union, List
+from pathlib import Path 
+
+
+
+class Scorer(ABC): 
+    """ 
+    A Scorer scores a reaction. The score should indicate
+    the likelihood that a reaction will be successful. These scores are maximized 
+    in the route optimization workflow. 
+
+    """
+    @abstractmethod
+    def score_rxn(rxn_smi: str, conditions: List = None) -> Union[float, int]:
+        """ Calculates a reaction score """
+
+    @abstractproperty
+    def requires_contexts() -> bool:
+        """ Whether the scorer needs contexts (i.e., conditions) to score a reaction """
+
+    def __call__(self, rxn_smi, conditions: List = None) -> float: 
+        self.score_rxn(rxn_smi, conditions)
+    
+
+class AskcosScorer(Scorer): 
+    """ 
+    Scores reactions using ASKCOS 
+    """
+    def __init__(self, 
+                 context_recommender = None, 
+                 ): 
+        
+        from askcos.synthetic.evaluation.evaluator import Evaluator
+
+        self.requires_contexts = True 
+        self.scorer = Evaluator()
+    
+    def score_rxn(self, rxn_smi: str, condition: List = None) -> float: 
+        """ Scores a single reaction with a single set of conditions ('condition') """
+        reactant_smi, product_smi = rxn_smi.split('>>')
+
+        evaluation_results = self.scorer.evaluate(
+            reactant_smiles=reactant_smi, 
+            target=product_smi, 
+            contexts=[condition],
+        )
+
+        score = evaluation_results[0]['target']['prob']
+
+        return score 
+    
+
+class LookupScorer(Scorer): 
+    """ Scores reactions by looking them up in a stored file """
+    def __init__(self, 
+                 lookup_file: Union[str, Path], 
+                 descriptor_type: str, 
+                 ):
+
+        self.requires_contexts = False 
+
+        return 
