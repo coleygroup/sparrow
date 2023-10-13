8bd19cd568b76a3a485b78a7ddfa57244262fcb2
jenna-fromer <jenna.fromer@gmail.com>
2023-10-13 15:20:56 -0400

    remove circular dependencies

--- src/sparrow/__init__.py
+++ src/sparrow/__init__.py
@@ -2,5 +2,3 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"]='-1'
 
 from .nodes import Node, CompoundNode, ReactionNode
-from .scorer import Scorer, AskcosScorer, LookupScorer
-from .condition_recommender import Recommender, AskcosRecommender
