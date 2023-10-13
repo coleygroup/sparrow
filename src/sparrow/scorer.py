9204152a232892fbd31ed24f7d6f3ad158727f0f
jenna-fromer <jenna.fromer@gmail.com>
2023-10-13 15:21:27 -0400

    Implement/debug ASKCOS API condition recommender

--- src/sparrow/scorer.py
+++ src/sparrow/scorer.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import requests
 from rdkit import Chem
 
+from sparrow.api_utils import post_and_get
+
 
 def canonicalize(smiles: str) -> str: 
     return Chem.MolToSmiles(Chem.MolFromSmiles(smiles), isomericSmiles=False)
@@ -30,7 +32,11 @@ class AskcosScorer(Scorer):
     """ 
     Scores reactions using local ASKCOS 
     """
-    def __init__(self): 
+    def __init__(self, askcos_path = None):
+
+        if askcos_path is not None: 
+            import sys
+            sys.path.append(askcos_path) 
         
         from askcos.synthetic.evaluation.evaluator import Evaluator
 
@@ -61,17 +67,29 @@ class AskcosAPIScorer(Scorer):
         self.host = host 
     
     def score_rxn(self, rxn_smi: str, condition: List = None) -> float:
+        if type(condition[0]) is list: 
+            condition = condition[0]
         reactants, product = rxn_smi.split('>>')
 
         params = {
             'reactants': reactants, 
             'reagents': '.'.join(condition),
-            'num_results': 10 
+            'num_results': 10, 
         }
-        resp = requests.get(self.host+'/api/forward/', params=params, verify=False)
+        try:
+            _, result = post_and_get(
+                host_post=self.host+'/api/forward/', 
+                host_results=self.host+'/api/v2/celery/',
+                params=params,
+                sleep_time=1,
+                timeout=60,
+            )
+        except ConnectionError:
+            print ("Connection Error from " + self.host)
+
         outcomes = {
-            prod['smiles']: prod['score']
-            for prod in resp.json()['outcomes']
+            prod['smiles']: prod['prob']
+            for prod in result['output']
         }
         if canonicalize(product) in outcomes: 
             return outcomes[product]
