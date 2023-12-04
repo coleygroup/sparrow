16a438636b29a4f85d6733d050fff318098418d9
Jenna Fromer <jfromer@mit.edu>
2023-12-04 13:07:07 -0500

    initial commit

---
+++ src/sparrow/utils/mmp_utils.py
@@ -0,0 +1,12 @@
+""" 
+Finds all matched molecular pairs in a list of SMILEs using __ algorithm 
+"""
+
+def find_mmps(smiles: list): -> list
+    """ 
+    Takes in a list of SMILES strings
+    Outputs a list of tuples with all matched molecular pairs in the set 
+    """
+    # TODO: actually implement this function
+    mmps = []
+    return mmps 
