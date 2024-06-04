629a266fcea0a380a626585108b86d03fd2f1569
Jenna Fromer <jfromer@mit.edu>
2024-06-04 10:49:56 -0400

    add run from config file for easier debugging

---
+++ scripts/run_from_config.py
@@ -0,0 +1,34 @@
+from sparrow.cli.run import * 
+
+path_to_config = ''
+
+
+def run_from_config(path_to_config):
+    params = vars(get_args("--config " + path_to_config))
+
+    print('SPARROW will be run with the following parameters:')    
+    for k, v in sorted(params.items()):
+        if v is not None: 
+            print(f"  {k}: {v}")
+    print(flush=True)
+    output_dir = Path(params['output_dir'])
+    output_dir.mkdir(exist_ok=True, parents=True)
+    
+    save_args(params)
+
+    target_dict, targets = get_target_dict(params['target_csv']) 
+    if params['diversity_weight'] > 0: 
+        print(f'Overwriting parameter "--cluster" to be similarity for diversity objective')
+        params['cluster'] = 'similarity'
+    clusters = get_clusters(
+        cluster_type=params['cluster'], 
+        filepath=params['target_csv'], 
+        cutoff=params['cluster_cutoff'], 
+        outdir=params['output_dir']
+    )
+    storage_path = get_path_storage(params, targets)
+    selector = build_selector(params, target_dict, storage_path, clusters)
+    selector = optimize(selector, params)
+
+if __name__=='__main__':
+    run_from_config(path_to_config)
